07b3e7979340475d32c33a33e8772fb67894a5ab
root <root@jared-spectre.localdomain>
2018-05-23 17:19:53 -0400

    add wins by clicking on winning player

--- README.md
+++ README.md
@@ -1,14 +1,52 @@
-## Resistance Calculation
+POM (Packala Open Manager) is a lightweight TOM clone written in React for people who don't have access to TOM or would prefer something more lightweight. POM is only to be used for *unsanctioned* events.
 
-Calculation formula pulled from [this thread](http://pokegym.net/community/index.php?threads/tournament-resistance-calculation.29506/).
+## Data Structures
 
 Each player is stored as follows:
 
 ```javascript
 player = {
     name: 'Jared',
-    wins: 9,
+    wins: 4,
     ties: 0,
-    losses: 0
+    losses: 0,
+    played: ["Russell LaParre", "Rahul Reddy", "Chris Schemanske", "Kenward"]
+}
+```
+
+Note: Players in the "played" array are references to other players, instead of plain strings.
+
+For pairings, the array is sorted by match points first (calculated below), then resistance (also calculated balow).
+
+## Calculations
+
+Match points are calculated as you'd expect:
+
+```javascript
+let matchPoints = player => player.wins * 3 + player.ties;
+```
+
+The resistance calculation (formula pulled from [this thread](http://pokegym.net/community/index.php?threads/tournament-resistance-calculation.29506/)) is the average of all played opponents' win percentages. Each win percentage is calculated by averaging the wins, losses, and ties over the number of matches played (ties count as half of a win). This is done as follows:
+
+```javascript
+let winPercentage = player => (player.wins + player.ties / 2) / (player.losses);
+```
+
+From this method, we can define a particular player's resistance as follows:
+
+```javascript
+let resistance = player => {
+    // Resistance sum starts off as zero
+    let resistanceTotal = 0;
+                              
+    // Add the win percentages of each player
+    for (let p in player.played)
+        resistanceTotal += winPercentage(player.played[p]);
+    
+    // Divide by total number of players. This gets the average win percentage
+    resistanceTotal /= player.played.length;
+                            
+    // This outputs a raw decimal. For display, multiply it by 100 and fix it at 2 decimal places.
+    return resistanceTotal;
 }
 ```
--- src.jsx
+++ src.jsx
@@ -12,7 +12,7 @@ class Initialize extends React.Component {
     
     addPlayer() {
         let players = this.state.players;
-        players.push({name: this.state.newPlayer, wins: 0, ties: 0, losses: 0});
+        players.push({name: this.state.newPlayer, wins: 0, ties: 0, losses: 0, played: []});
         this.setState({players});
     }
     
@@ -38,67 +38,146 @@ class Initialize extends React.Component {
     }
 }
 
-function gameResult(winningPlayer, losingPlayer) {
-    alert();
-    winningPlayer.wins++;
-    losingPlayer.losses++;
-}
+class GeneratePairings extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {players: props.players};
+            let tempPlayers = JSON.parse(JSON.stringify(props.players));
 
-function displayPlayer(p) {
-    if (p == "bye")
-        return <div class="col-sm" id="bye">BYE</div>;
-    return (
-        <div class="col-sm" id={p.name} onclick="alert()">{p.name + " (" + p.wins + "-" + p.ties + "-" + p.losses + ")"}</div>
-    );
-}
+            while (tempPlayers.length > 0) {
+                let firstPlayerPos = Math.floor(Math.random() * tempPlayers.length);
+                let firstPlayer = findPlayer(tempPlayers[firstPlayerPos].name);
+                tempPlayers.splice(firstPlayerPos, 1);
 
-function GeneratePairings(props) {
-        while (props.players.length > 0) {
-            let firstPlayerPos = Math.floor(Math.random() * props.players.length);
-            let firstPlayer = props.players[firstPlayerPos];
-            props.players.splice(firstPlayerPos, 1);
+                // This is where we manipulate the bye
+                if (tempPlayers.length == 0) {
+                    firstPlayer.wins++;
+                    pairings.push({first: firstPlayer, second: "bye"});
+                }
+                
+                else {
+                    let secondPlayerPos = Math.floor(Math.random() * tempPlayers.length);
+                    let secondPlayer = findPlayer(tempPlayers[secondPlayerPos].name);
+                    tempPlayers.splice(secondPlayerPos, 1);
 
-            // This is where we manipulate the bye
-            if (props.players.length == 0)
-                pairings.push({first: firstPlayer, second: "bye"});
-            else {
-                let secondPlayerPos = Math.floor(Math.random() * props.players.length);
-                let secondPlayer = props.players[secondPlayerPos];
-                props.players.splice(secondPlayerPos, 1);
-
-                pairings.push({first: firstPlayer, second: secondPlayer});
+                    pairings.push({first: firstPlayer, second: secondPlayer});
+                }
             }
+
+            this.displayPlayer = this.displayPlayer.bind(this);
+            this.handleClick = this.handleClick.bind(this);
+        }
+    
+        displayPlayer(p) {
+            if (p == "bye")
+                return <div class="col-sm" id="bye">BYE</div>;
+            return (
+                <div class="col-sm" id={p.name} onClick={(e) => this.handleClick(e)}>{p.name + " (" + p.wins + "-" + p.ties + "-" + p.losses + ") " + resistanceDisplay(p) + "%"}</div>
+            );
+        }
+    
+        handleClick(e) {
+            // Make sure we don't already have a result
+            if (e.target.parentElement.classList.contains('round-complete'))
+                return;
+            
+            // Say we have a result
+            e.target.parentElement.classList.add('round-complete');
+            
+            // Find the name of the next player
+            let nextPlayer;
+            if (e.target.previousSibling == null)
+                nextPlayer = e.target.nextSibling.nextSibling.id;
+            else
+                nextPlayer = e.target.previousSibling.previousSibling.id;
+            
+            // We don't care about byes
+            if (nextPlayer == "bye")
+                return;
+            
+            // Convert the names of the two players into objects
+            let thisPlayerObj = findPlayer(e.target.id);
+            let nextPlayerObj = findPlayer(nextPlayer);
+
+            // Don't add more than one match result
+            if (thisPlayerObj.played.includes(nextPlayerObj))
+                return;
+            
+            // Adds win/losses to player objects            
+            thisPlayerObj.wins++;
+            nextPlayerObj.losses++;
+            
+            // Marks that each player has played one another
+            thisPlayerObj.played.push(nextPlayerObj);
+            nextPlayerObj.played.push(thisPlayerObj);
+            
+            this.setState({players: players});
         }
 
-        return (
-            <div>{pairings.map(p => 
-                <div class="row">
-                    {displayPlayer(p.first)}
-                    <div class="col-sm"> vs </div>
-                    {displayPlayer(p.second)}
-                </div>
-            )}</div>
-        );
+        render() {
+            return (
+                <div>{pairings.map(p => 
+                    <div class="row">
+                        {this.displayPlayer(p.first)}
+                        <div class="col-sm"> vs </div>
+                        {this.displayPlayer(p.second)}
+                    </div>
+                )}</div>
+            );
+        }
     }
 
 class Pairings extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {round: 1};
+        this.nextRound = this.nextRound.bind(this);
+    }
+    
+    nextRound() {
+        let r = this.state.round + 1;
+        this.setState({round: r});
     }
     
     render() {   
         return (
             <div>
+                <h1 id="round-number">Round {this.state.round}</h1>
                 <GeneratePairings players={players} />
-                <div id="won"></div>
+                <button onClick={() => this.nextRound()}>Next Round</button>
             </div>
         );
     }
 }
     
 
-let players = [{name: 'Jared', wins: 0, ties: 0, losses: 0}, {name: 'Kenward', wins: 0, ties: 0, losses: 0}];
+let players = [{name: 'Jared', wins: 6, ties: 0, losses: 0, played: []}, {name: 'Kenward', wins: 3, ties: 0, losses: 3, played: []}];
 let pairings = [];
+
+let findPlayer = (name) => {
+    for (let p in players)
+        if (players[p].name == name)
+            return players[p];
+};
+
+let matchPoints = player => player.wins * 3 + player.ties;
+
+let winPercentage = player => (player.wins + player.ties / 2) / (player.wins + player.ties + player.losses);
+
+let resistance = (player) => {
+    let resistanceTotal = 0;
+    for (let p in player.played)
+        resistanceTotal += winPercentage(player.played[p]);
+    
+    resistanceTotal /= player.played.length;
+    return resistanceTotal;
+}
+
+let resistanceDisplay = player => resistance(player).toFixed(4) * 100;
+
+function comparePlayers(thisPlayer, nextPlayer) {
+    return matchPoints(thisPlayer) < matchPoints(nextPlayer);
+}
 
 ReactDOM.render(
     <Initialize players={players} />,
